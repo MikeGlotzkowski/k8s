4b736ce7f9b334bcb38f868a24b60381605e53a0
MikeGlotzkowski <sebastian.lischewski@outlook.com>
2020-07-26 16:37:55 +0200

    feature(docker registry): move to docker hub and github workflows

--- dot-net-core/kafka_simple/KafkaConsumer/build.sh
+++ dot-net-core/kafka_simple/KafkaConsumer/build.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-IMAGE_NAME='kafkaconsumer-dot-net-core'
+IMAGE_NAME='sebilhd/kafkaconsumer-dot-net-core'
 
 for tag in $(docker images | grep ${IMAGE_NAME} | awk '{print $2}'); do docker rmi --force ${IMAGE_NAME}:${tag}; done
 docker build -t ${IMAGE_NAME}:0.0.3 .
--- python/kafka_simple/kafka_consumer/build.sh
+++ python/kafka_simple/kafka_consumer/build.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-IMAGE_NAME='kafkaconsumer-python'
+IMAGE_NAME='sebilhd/kafkaconsumer-python'
 
 for tag in $(docker images | grep ${IMAGE_NAME} | awk '{print $2}'); do docker rmi --force ${IMAGE_NAME}:${tag}; done
 docker build -t ${IMAGE_NAME}:0.0.16 .
