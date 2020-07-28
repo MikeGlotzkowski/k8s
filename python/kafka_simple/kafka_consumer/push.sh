258ff668d6eea68bc963a22f8a527557afcb96e7
MikeGlotzkowski <sebastian.lischewski@outlook.com>
2020-07-28 20:50:47 +0200

    feature(cicd): add build-push for python consumer for gocd

--- python/kafka_simple/kafka_consumer/push.sh
+++ python/kafka_simple/kafka_consumer/push.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
 
-# echo "$DOCKER_SECRET" | docker login --username $DOCKER_USER --password-stdin
-docker login -u $DOCKER_USER -p $DOCKER_SECRET
+echo "$DOCKER_SECRET" | docker login --username $DOCKER_USER --password-stdin
+# docker login -u $DOCKER_USER -p $DOCKER_SECRET
 docker push ${IMAGE_NAME}:${IMAGE_VERSION}
