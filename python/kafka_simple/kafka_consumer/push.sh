b7d3c92106ffda8c04139519e03f752361a15691
MikeGlotzkowski <sebastian.lischewski@outlook.com>
2020-07-28 20:42:11 +0200

    fix(cicd): use different command to login to docker registry

--- python/kafka_simple/kafka_consumer/push.sh
+++ python/kafka_simple/kafka_consumer/push.sh
@@ -1,4 +1,5 @@
 # !/bin/bash
 
-echo "$DOCKER_SECRET" | docker login --username $DOCKER_USER --password-stdin
+# echo "$DOCKER_SECRET" | docker login --username $DOCKER_USER --password-stdin
+docker login -u $DOCKER_USER -p $DOCKER_SECRET
 docker push ${IMAGE_NAME}:${IMAGE_VERSION}
