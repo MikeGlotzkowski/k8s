3c13716e585c2b95f71b8a7f947110b309e92968
MikeGlotzkowski <sebastian.lischewski@outlook.com>
2020-07-28 21:09:57 +0200

    feature(cicd): test cicd pipeline on k8s gocd with elastic agents; disable github workflow

--- python/kafka_simple/kafka_consumer/build.sh
+++ python/kafka_simple/kafka_consumer/build.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
 export IMAGE_NAME='sebilhd/kafkaconsumer-python'
-export IMAGE_VERSION='0.0.17'
+export IMAGE_VERSION='0.0.18'
 # for tag in $(docker images | grep ${IMAGE_NAME} | awk '{print $2}'); do docker rmi --force ${IMAGE_NAME}:${tag}; done
 docker build -t ${IMAGE_NAME}:${IMAGE_VERSION} .
