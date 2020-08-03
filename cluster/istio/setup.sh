261a5246370c26676adee1018da84f7b2d63ed48
MikeGlotzkowski <sebastian.lischewski@outlook.com>
2020-08-03 21:10:56 +0200

    feature(istio): add variables for dashboards

--- cluster/istio/setup.sh
+++ cluster/istio/setup.sh
@@ -10,4 +10,13 @@ istioctl install --set profile=$ISTIO_PROFILE
 kubectl label namespace $ISTIO_USE_IN_NS istio-injection=enabled
 istioctl verify-install
 istioctl analyze
+kubectl get svc istio-ingressgateway -n istio-system
+export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+export INGRESS_HOST="127.0.0.1" #override "localhost"
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
+export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+echo $GATEWAY_URL
+
+
 
